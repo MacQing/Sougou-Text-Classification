b82fb4e14f9ddd33448814f1c8e8e0f3883e84d6
MacQing <medichen@foxmail.com>
2018-05-20 15:03:20 +0800

    将新闻数据导入到ES

---
+++ StringUtils.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/20 13:59
+# @Author  : yindaqing
+# @Email   : medichen@foxmail.com
+# @File    : StringUtils.py
+# @Software: PyCharm
---
+++ index2es.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/5/20 12:36
+# @Author  : yindaqing
+# @Email   : medichen@foxmail.com
+# @File    : index2es.py
+# @Software: PyCharm
